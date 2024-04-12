de2562134e569e0efebc17e8db3fdaa8966236d7
caua-dev-coder <cauatavellaprofissional@gmail.com>
2024-04-12 20:09:11 -0300

    Add Scroll Review

---
+++ src/JS/scritpt.js
@@ -0,0 +1,7 @@
+window.sr = ScrollReveal ({reset: true});
+
+sr.reveal(".home", {duration: 1500})
+sr.reveal(".sobre-mim section", {duration: 1500})
+sr.reveal(".habilidades section", {duration: 1500})
+sr.reveal(".projetos section", {duration: 1500})
+sr.reveal(".footer", {duration: 1500})
